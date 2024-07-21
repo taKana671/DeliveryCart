15759d9ac7d86bbe8d7d9b1c8e668394f7164f25
taKana671 <kanae5321@gmail.com>
2024-07-22 00:31:14 +0900

    Changed to use my own car.

--- parking_practice.py
+++ parking_practice.py
@@ -1,8 +1,13 @@
+import sys
+
 from panda3d.bullet import BulletWorld, BulletDebugNode
-from panda3d.core import Vec3
+from panda3d.core import Vec3, Point3, NodePath
+
 from direct.showbase.ShowBase import ShowBase
 from direct.showbase.ShowBaseGlobal import globalClock
 
+from car import Car
+from my_car import Cart
 from scene import Scene
 
 
@@ -11,8 +16,6 @@ class PracticeParking(ShowBase):
     def __init__(self):
         super().__init__()
         self.disable_mouse()
-        self.camera.set_pos(0, -100, 100)
-        self.camera.look_at(0, 0, 0)
 
         self.world = BulletWorld()
         self.world.set_gravity(Vec3(0, 0, -9.81))
@@ -20,15 +23,39 @@ class PracticeParking(ShowBase):
         self.debug = self.render.attach_new_node(BulletDebugNode('debug'))
         self.world.set_debug_node(self.debug.node())
         self.debug.show()
-        self.scene = Scene()
+        self.scene = Scene(self.world)
+        # self.car = Car(self.world)
+        self.car = Cart(self.world)
+        self.car.reparent_to(self.render)
 
+        self.floater = NodePath('floater')
+        self.floater.set_pos(Point3(0, 0, 2))
+        self.floater.reparent_to(self.car)
+
+        self.camera.set_pos(self.car.get_pos() + Vec3(0, -10, 3))
+        # self.camera.look_at(0, 0, 0)
+        # self.camera.set_pos(0, -10, 3)
+        # self.camera.reparent_to(self.car)
+        self.camera.look_at(self.floater)
+
+        self.accept('escape', sys.exit)
+        self.accept('d', self.toggle_debug)
         self.taskMgr.add(self.update, 'update')
+
+    def toggle_debug(self):
+        if self.debug.is_hidden():
+            self.debug.show()
+        else:
+            self.debug.hide()
 
     def update(self, task):
         dt = globalClock.get_dt()
+        # self.process_input(dt)
+        self.car.control(dt)
 
         self.world.do_physics(dt)
         return task.cont
+
 
 
 if __name__ == '__main__':
--- scene.py
+++ scene.py
@@ -1,6 +1,40 @@
 from panda3d.bullet import BulletRigidBodyNode
-from panda3d.bullet import BulletPlaneShape
-from panda3d.core import NodePath, PandaNode, CardMaker, BitMask32, Vec3
+from panda3d.bullet import BulletPlaneShape, BulletHeightfieldShape, ZUp
+from panda3d.core import NodePath, PandaNode, CardMaker, BitMask32, Vec3, Point3
+from panda3d.core import Filename, PNMImage
+# from panda3d.core import GeoMipTerrains
+
+from parking_lot import ParkingLot
+
+
+# class Terrain(NodePath):
+
+#     def __init__(self):
+#         super().__init__(BulletRigidBodyNode('terrain'))
+#         self.height = 10
+#         self.set_pos(Point3(0, 0, 0))
+#         self.node().set_mass(0)
+#         self.set_collide_mask(BitMask32.bit(1))
+
+#         img = PNMImage(Filename('terrains/heightfield7.png'))
+#         shape = BulletHeightfieldShape(img, self.height)
+#         shape.set_use_diamond_subdivision(True)
+#         self.node().add_shape(shape)
+
+#         self.terrain = GeoMipTerrains('geomip_terrain')
+#         self.terrain.set_heightfield('terrains/heightfield7.png')
+#         self.terrain.set_block_size(8)
+#         self.terrain.set_min_level(2)
+#         self.terrain.set_focal_point(base.camera)
+
+#         self.root = self.terrain.get_root()
+#         self.root.set_scale(Vec3(1, 1, self.height))
+#         # self.root.set_pos()
+#         self.terrain.generate()
+#         self.root.reparent_to(self)
+
+        
+
 
 
 class Ground(NodePath):
@@ -9,7 +43,8 @@ class Ground(NodePath):
         super().__init__(BulletRigidBodyNode('ground'))
         model = self.create_ground()
         model.reparent_to(self)
-        self.set_collide_mask(BitMask32.all_on())
+        # self.set_collide_mask(BitMask32.all_on())
+        self.set_collide_mask(BitMask32.bit(1))
         self.node().add_shape(BulletPlaneShape(Vec3.up(), 0))
 
     def create_ground(self):
@@ -17,8 +52,8 @@ class Ground(NodePath):
         card = CardMaker('card')
         card.set_frame(-1, 1, -1, 1)
 
-        for y in range(50):
-            for x in range(50):
+        for y in range(-25, 25):
+            for x in range(-25, 25):
                 g = model.attach_new_node(card.generate())
                 g.set_p(-90)
                 g.set_pos(x, y, 0)
@@ -33,8 +68,11 @@ class Ground(NodePath):
 
 class Scene(NodePath):
 
-    def __init__(self):
+    def __init__(self, world):
         super().__init__(PandaNode('scene'))
-        ground = Ground()
-        ground.reparent_to(self)
-        self.reparent_to(base.render)
+        self.world = world
+        # ground = Ground()
+        # ground.reparent_to(self)
+        # self.reparent_to(base.render)
+        # world.attach(ground.node())
+        self.parking_lot = ParkingLot(self.world)
